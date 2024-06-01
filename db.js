07044583f4aa8b731f5d463f53f57ad14f1539b1
Lucas Macedo Bernardino <lucasmacedoes@gmail.com>
2024-06-01 19:35:43 -0300

    update db config

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: "default",
+  host: "ep-green-frog-a43mj1ha-pooler.us-east-1.aws.neon.tech",
+  database: "verceldb",
+  password: "1HChEVbp3UvR",
+  port: 5432,
 });
 
 module.exports = pool;
