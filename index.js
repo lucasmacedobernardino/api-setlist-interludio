e5d3cdd827d1ad2e2b6e8d1f1e2152329a43e464
lucasmacedoes@gmail.com <lucasmacedoes@gmail.com>
2024-06-01 18:43:32 -0300

    Vercel file

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.get('/setlist', async (req, res) => {
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
